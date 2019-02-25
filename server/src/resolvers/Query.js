32ecbca41f00a73ca819cedc2b78ec7bca386a19
SeungHo Han <furtherdream.ceo@gmail.com>
2019-02-25 22:16:40 +0900

    GraphQL - Paging

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,12 +1,23 @@
-async function feed(parent, args, ctx, info) {
-  const { filter, first, skip } = args // destructure input arguments
-  const where = filter ? { OR: [{ url_contains: filter }, { description_contains: filter }] } : {}
-
-  const queriedLinks = await ctx.db.query.links({ first, skip, where })
-
+async function feed(parent, args, context) {
+  const count = await context.prisma
+    .linksConnection({
+      where: {
+        OR: [{ description_contains: args.filter }, { url_contains: args.filter }]
+      }
+    })
+    .aggregate()
+    .count()
+  const links = await context.prisma.links({
+    where: {
+      OR: [{ description_contains: args.filter }, { url_contains: args.filter }]
+    },
+    skip: args.skip,
+    first: args.first,
+    orderBy: args.orderBy
+  })
   return {
-    linkIds: queriedLinks.map(link => link.id),
-    count
+    count,
+    links
   }
 }
 
