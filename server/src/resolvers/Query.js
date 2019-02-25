58aea333046323dda0eda5721d6721c2c79e5689
SeungHo Han <furtherdream.ceo@gmail.com>
2019-02-25 17:57:35 +0900

    GraphQL - filtering

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,32 +1,15 @@
-async function feed(parent, args, context) {
-  const count = await context.prisma
-    .linksConnection({
-      where: {
-        OR: [
-          { description_contains: args.filter },
-          { url_contains: args.filter },
-        ],
-      },
-    })
-    .aggregate()
-    .count()
-  const links = await context.prisma.links({
-    where: {
-      OR: [
-        { description_contains: args.filter },
-        { url_contains: args.filter },
-      ],
-    },
-    skip: args.skip,
-    first: args.first,
-    orderBy: args.orderBy,
-  })
+async function feed(parent, args, ctx, info) {
+  const { filter, first, skip } = args // destructure input arguments
+  const where = filter ? { OR: [{ url_contains: filter }, { description_contains: filter }] } : {}
+
+  const queriedLinks = await ctx.db.query.links({ first, skip, where })
+
   return {
-    count,
-    links,
+    linkIds: queriedLinks.map(link => link.id),
+    count
   }
 }
 
 module.exports = {
-  feed,
+  feed
 }
